876a85bd15a0a69a26150ab77f3ba9d5659374aa
tesujiro <tesujiro@gmail.com>
2018-10-17 18:12:43 +0900

    add ajax call

--- web/server.go
+++ web/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -39,12 +40,37 @@ func main() {
 
 func (s *server) routes() {
 	s.router.HandleFunc("/greet", s.handleHello())
+	s.router.HandleFunc("/cities", s.handleCities())
 	//s.router.HandleFunc("/api/flyer", s.handleFlyer())
 	s.router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
 }
 
 func (s *server) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("handlerHello")
 		fmt.Fprintf(w, "Hello, World")
+	}
+}
+
+type jsmap map[string]interface{}
+
+func (s *server) handleCities() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("handlerCities")
+		w.Header().Set("Content-Type", "application/json")
+		list := []jsmap{
+			{"name": "Setagaya", "population": 925226},
+			{"name": "Nerima", "population": 733150},
+			{"name": "Ota", "population": 731273},
+			{"name": "Edogawa", "population": 691417},
+			{"name": "Adachi", "population": 678686},
+			{"name": "Suginami", "population": 577903},
+		}
+		m := jsmap{"list": list}
+		j, err := json.Marshal(m)
+		if err != nil {
+			fmt.Printf("json.Marshall error:%v\n", err)
+		}
+		fmt.Fprintf(w, string(j))
 	}
 }
